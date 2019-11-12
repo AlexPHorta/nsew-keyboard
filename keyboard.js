e99f74c163c8cad9ba8f5aef45138cfbc665995a
Itwilloutliveyou <alexandre.horta@gmail.com>
2019-11-11 23:14:57 -0300

    Constructing the grid, with tests.

--- keyboard.js
+++ keyboard.js
@@ -1,21 +1,47 @@
 function makeGrid(conf) {
     var confIsArray = conf instanceof Array;
+    var cell = {
+        colspan: 0,
+        id: null,
+        _class: null
+    };
+    var gridContents = [];
 
     if (!confIsArray) {
         throw new Error('Error');
     }
-    else {
-        var confLen = conf.length;
-        if (confLen == 0) {
-            return "<table></table>";
+
+    for (el in conf) {
+        var elem = conf[el];
+        if (elem.length==0) {
+            gridContents.push("<tr></tr>");
         }
-        else if (confLen == 1){
-            if (conf[0] === ""){
-                return "<table><tr></tr></table>";
+        else {
+            gridContents.push("<tr>");
+            var cSpan = 0;
+            var cellNum = 0;
+            for (c in elem) {
+                if (elem[c]=="-") {
+                    cSpan++;
+                }
+                cellNum++;
             }
-            else if (conf[0] === "-"){
-                return "<table><tr><td></td></tr></table>";
+            if (cSpan>1) {
+                var row = "<td colspan=\"" + cSpan + "\"></td>";
             }
+            else {
+                for (var c = cellNum;c>0;c--) {
+                    gridContents.push("<td></td>");
+                }
+            }
+
+            gridContents.push(row);
+
+            gridContents.push("</tr>");
+
         }
     }
+
+    return "<table>" + gridContents.join("") + "</table>";
+
 }
