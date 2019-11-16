7187d3e9e7b77ac15c4f13c60575312a393a36d5
Itwilloutliveyou <alexandre.horta@gmail.com>
2019-11-16 01:02:54 -0300

    Ready for the numbers and letters!"

--- keyboard.js
+++ keyboard.js
@@ -1,128 +1,56 @@
 function makeGrid(conf) {
+    // Make the table that will serve as the keyboard's structure from an array
+    // of characters that indicate the layout, e.g.:
+    //
+    // "-----8-----" --> <td colspan="5"></td><td id="8" class="key></td><td colspan="5"></td>
+    // "-----4t_---" --> <td colspan="5"></td><td id="4" class="key colspan="2"></td><td colspan="3"></td>
+    //
+    // Hopefully this will make it easier to rearrange the keyboard's layout
+    // simply modifying the configuration array.
+
     var confIsArray = conf instanceof Array;
-    var cell = {
-        colspan: 0,
-        id: null,
-        _class: null
-    };
     var gridContents = [];
 
     if (!confIsArray) {
         throw new Error('Error');
     }
 
-    for (el in conf) {
-        var elem = conf[el];
-        // var elemLength = elem.length;
-        var type = 0;
-        var count = 0;
+    function Cell (elem, quant) {
+        this.elem = elem;
+        this.quant = quant;
+        this.incr = function () {
+            this.quant++;
+        };
+    }
+
+    function mkCells (line) {
+        var objs = [];
+        for (c in line) {
+            if (line[c] !== line[c-1]) {
+                var ob = new Cell(line[c], 1);
+                objs.push(ob);
+            }
+            else { 
+                objs[objs.length-1].incr();
+            }
+        }
+        return objs;
+    }
+
+    for (ln in conf) {
         gridContents.push("<tr>");
-        for (c in elem) {
-            count++;
-            type = elem[c];
-            if (elem[c]===elem[c-1]) {
-                console.log("a", conf, type, count);
-            }
-            else {
-                //count = 1;
-                console.log("b", conf, type, count);
-            }
-            if (c==elem.length-1) {
-                if (elem[c]==="-") {
-                    if (count==1) {
-                        gridContents.push("<td></td>");
-                    }
-                    else if (count>1) {
-                        var dSpan = "<td colspan=\"" + count + "\"></td>";
-                        gridContents.push(dSpan);
-                    }
-                }
-                if (elem[c]==="*") {
-                    if (count==1) {
-                        gridContents.push("<td></td>");
-                    }
-                    else if (count>1) {
-                        for (var c=count; c>0; c--) {
-                            gridContents.push("<td></td>");
-                        }
-                   }
+        var lin = mkCells(conf[ln]);
+        for (ob in lin) {
+            if (lin[ob].quant === 1 || lin[ob].elem === "*"){
+                for (var i=lin[ob].quant; i>0; i--) {
+                    gridContents.push("<td></td>");
                 }
             }
-            if (elem[c]!==elem[c-1] && c>0 && c<elem.length-1) {
-                count--;
-                if (elem[c-1]==="-") {
-                    if (count==1) {
-                        gridContents.push("<td></td>");
-                    }
-                    else if (count>1) {
-                        var dSpan = "<td colspan=\"" + count + "\"></td>";
-                        gridContents.push(dSpan);
-                    }
-                }
-                if (elem[c-1]==="*") {
-                    if (count==1) {
-                        gridContents.push("<td></td>");
-                    }
-                    else if (count>1) {
-                        for (var c=count; c>0; c--) {
-                            gridContents.push("<td></td>");
-                        }
-                    }
-                }
-                count = 1;
+            else if (lin[ob].elem === "-") {
+                gridContents.push("<td colspan=\"" + lin[ob].quant + "\"></td>");
             }
         }
         gridContents.push("</tr>");
     }
-//        if (elemLength==0) {
-//            gridContents.push("<tr></tr>");
-//        }
-//        else {
-//            gridContents.push("<tr>");
-//            var numCells = 0;
-//            var cellType = "";
-//            var prev = "";
-//            for (c in elem) {
-//                if (prev==="") {
-//                    cellType = elem[c];
-//                    numCells++;
-//                    console.log(c, cellType, numCells, prev);
-//                }
-//                if (elem[c]===prev) {
-//                    numCells++;
-//                }
-//                if (c==elemLength-1) {
-//                    if (elem[c]!==prev) {
-//                        numCells = 1;
-//                        cellType = elem[c];
-//                        if (cellType==="-" || cellType==="*") {
-//                            gridContents.push("<td></td>");
-//                        }
-//                    }
-//                    else {
-//                        if (prev==="*") {
-//                            for (var c=numCells; c>0; c--) {
-//                                gridContents.push("<td></td>");
-//                            }
-//                        }
-//                        if (prev==="-") {
-//                            var dSpan = "<td colspan=\"" + numCells + "\"></td>";
-//                            gridContents.push(dSpan);
-//                        }
-//                    }
-//                }
-//                else if (c<elemLength-1) {
-//                }
-//
-//                prev = elem[c];
-//
-//            }
-//
-//                gridContents.push("</tr>");
-//
-//        }
-//    }
-
     return "<table>" + gridContents.join("") + "</table>";
-
 }
