34bcb85b4c94154999d7ba02e598f0bf9ebc078f
Itwilloutliveyou <alexandre.horta@gmail.com>
2019-12-24 01:41:41 -0300

    This is working fine.

--- scripts/main.js
+++ scripts/main.js
@@ -21,14 +21,10 @@
  *
  ***************************************************************************/
 
-"use strict";
+// "use strict";
 
 
-class NSEWK {
-
-    constructor(){
-
-        this.layout =
+let NSEW_layout =
         [
         ["e", "E", ".", "1"], ["t", "T", ",", "7"], ["a", "A", ";", "0"],
         ["o", "O", ":", "4"], ["i", "I", "!", "2"], ["n", "N", "?", "8"],
@@ -42,7 +38,15 @@ class NSEWK {
         [" ", " ", " ", " "], ["Bksp", "Bksp", "Bksp", "Bksp"],
         ["Cls", "Cls", "Cls", "Cls"]];
 
-        this.routes =
+let meta = {
+    active: 0,
+    options: 4,
+    alter: function(){
+        this.active = (this.active + 1) % this.options;
+    }
+};
+
+let paths =
         [
         [4, 28, 26, 27], [12, 5, 13, 26], [26, 14, 6, 15],
         [17, 26, 16, 7], [8, 29, 0, 29],  [18, 9, 19, 1],
@@ -55,253 +59,98 @@ class NSEWK {
         [20, 25, 29, 10],[21, 10, 27, 24],[0, 1, 2, 3],
         [25, 0, 26, 28], [29, 27, 26, 0], [24, 4, 28, 4]];
 
-        this.meta = {
-        active: 0,
-        options: 4,
-        alter: function(){
-            this.active = (this.active + 1) % this.options;
-            }
-        };
+let controls = {
+    up: "8",
+    right: "6",
+    down: "2",
+    left: "4",
+    enter: "5"
+};
 
-        this.up = "8";
-        this.right = "6";
-        this.down = "2";
-        this.left = "4";
-        this.enter = "5";
+function populate(){
+    let charKeys = document.getElementsByClassName("char");
+    let numChars = charKeys.length;
+    let metaActive = meta.active;
 
-    }
-
-    populate(){
-        let charKeys = document.getElementsByClassName("char");
-        let numChars = charKeys.length;
-        let metaActive = this.meta.active;
-
-        for (let i = 0; i < numChars; i++){
-            // console.log(this.layout[Number(charKeys[i].id)][metaActive]);
-            charKeys[i].innerText = this.layout[Number(charKeys[i].id)][metaActive];
-        }
-    }
-
-    walk(event){
-        let press = String(event.key);
-        console.log(this.up);
-        let active = document.getElementsByClassName("active")[0];
-        let activeId = active.id;
-        let neighbor;
-        let selected = null;
-        let jumpto;
-
-        if (press === this.up){
-            neighbor = 0;
-        }
-        else if (press === this.right){
-            neighbor = 1;
-        }
-        else if (press === this.down){
-            neighbor = 2;
-        }
-        else if (press === this.left){
-            neighbor = 3;
-        }
-        else if (press === this.enter){
-            // console.log(this.layout[Number(activeId)]);
-            selected = this.layout[Number(activeId)];
-            this.select(selected);
-            if (selected.includes("Bksp")){
-                neighbor = 28;
-            }
-            else {
-                neighbor = 26;
-            }
-        }
-
-        if (neighbor == 26){
-            jumpto = neighbor;
-        }
-        else {
-            // console.log(this.routes[Number(activeId)][neighbor]);
-            jumpto = this.routes[Number(activeId)][neighbor];
-        }
-
-        if (jumpto >= 0){
-            this.draw(activeId, jumpto);
-        }
-    }
-
-    select(key){
-
-        if (key.includes("Meta")){
-            this.meta.alter();
-            this.populate();
-        }
-        else{
-            let key_ = key[this.meta.active];
-            let kbd = document.getElementsByClassName("NSEW_input")[0];
-            let kbdLen = kbd.length;
-        }
-
-        switch(key_){
-            case "Bksp":
-                kbd.value = kbd.value.slice(0,-1);
-                break;
-            case "Cls":
-                kbd.value = kbd.value.slice(0, kbdLen-1);
-                break;
-            default:
-                kbd.value += key;
-                break;
-        }
-    }
-
-    draw(here, whereto){
-        let h = Number(here);
-        let w = Number(whereto);
-
-        document.getElementById(h.toString()).className =
-        document.getElementById(h.toString()).className.replace( /(?:^|\s)active(?!\S)/g , '' );
-
-        document.getElementById(w.toString()).className += " active";
+    for (i = 0; i < numChars; i++){
+        charKeys[i].innerText = NSEW_layout[Number(charKeys[i].id)][metaActive];
     }
 }
 
-let keyboard = new NSEWK();
-keyboard.populate();
-document.addEventListener("keydown", keyboard.walk);
+function walk(event){
+    let press = event.key;
+    let active = document.getElementsByClassName("active")[0];
+    let activeId = active.id;
+    let neighbor;
+    let selected = null;
 
-// let NSEW_layout =
-//         [
-//         ["e", "E", ".", "1"], ["t", "T", ",", "7"], ["a", "A", ";", "0"],
-//         ["o", "O", ":", "4"], ["i", "I", "!", "2"], ["n", "N", "?", "8"],
-//         ["s", "S", "'", "¢"], ["h", "H", '"', "5"], ["r", "R", "-", "3"],
-//         ["d", "D", ")", "9"], ["l", "L", "@", "%"], ["c", "C", "(", "6"],
-//         ["u", "U", "#", "/"], ["m", "M", "$", ","], ["w", "W", "&", "-"],
-//         ["f", "F", "*", "+"], ["g", "G", "_", "."], ["y", "Y", "~", "*"],
-//         ["p", "P", "^", "|"], ["b", "B", "/", "º"], ["v", "V", "]", "¬"],
-//         ["k", "K", "[", "£"], ["j", "J", "<", "ª"], ["x", "X", ">", "\\"],
-//         ["q", "Q", "}", "="], ["z", "Z", "{", "°"], ["Meta", "Meta", "Meta", "Meta"],
-//         [" ", " ", " ", " "], ["Bksp", "Bksp", "Bksp", "Bksp"],
-//         ["Cls", "Cls", "Cls", "Cls"]];
+    if (press == controls.up){
+        neighbor = 0;
+    }
+    else if (press == controls.right){
+        neighbor = 1;
+    }
+    else if (press == controls.down){
+        neighbor = 2;
+    }
+    else if (press == controls.left){
+        neighbor = 3;
+    }
+    else if (press == controls.enter){
+        selected = NSEW_layout[Number(activeId)];
+        select(selected);
+        if (selected.includes("Bksp")){
+            neighbor = 28;
+        }
+        else {
+            neighbor = 26;
+        }
+    }
 
-// let meta = {
-//     active: 0,
-//     options: 4,
-//     alter: function(){
-//         this.active = (this.active + 1) % this.options;
-//     }
-// };
+    let jumpto;
 
-// let paths =
-//         [
-//         [4, 28, 26, 27], [12, 5, 13, 26], [26, 14, 6, 15],
-//         [17, 26, 16, 7], [8, 29, 0, 29],  [18, 9, 19, 1],
-//         [2, 20, 10, 21], [23, 3, 22, 11], [10, -1, 4, -1],
-//         [-1, 11, -1, 5], [6, 24, 8, 25],  [-1, 7, -1, 9],
-//         [28, 18, 1, 26], [1, 19, -1, 26], [26, 15, 20, 2],
-//         [26, 2, 21, 14], [3, 26, -1, 22], [27, 26, 3, 23],
-//         [28, 23, 5, 12], [5, 22, 18, 13], [14, 21, 24, 6],
-//         [15, 6, 25, 20], [7, 16, 23, 19], [22, 17, 7, 18],
-//         [20, 25, 29, 10],[21, 10, 27, 24],[0, 1, 2, 3],
-//         [25, 0, 26, 28], [29, 27, 26, 0], [24, 4, 28, 4]];
+    if (neighbor == 26){
+        jumpto = neighbor;
+    }
+    else {
+        jumpto = paths[Number(activeId)][neighbor];
+    }
 
-// let controls = {
-//     up: "8",
-//     right: "6",
-//     down: "2",
-//     left: "4",
-//     enter: "5"
-// };
+    if (jumpto >= 0){
+        draw(activeId, jumpto);
+    }
+}
 
-// function populate(){
+function select(key_){
+    let key = key_[meta.active];
+    let kbd = document.getElementsByClassName("NSEW_input")[0];
+    let kbdLen = kbd.length;
+    switch(key){
+        case "Meta":
+            meta.alter();
+            populate();
+            break;
+        case "Bksp":
+            kbd.value = kbd.value.slice(0,-1);
+            break;
+        case "Cls":
+            kbd.value = kbd.value.slice(0, kbdLen-1);
+            break;
+        default:
+            kbd.value += key;
+            break;
+    }
+}
 
-//     let charKeys = document.getElementsByClassName("char");
-//     let numChars = charKeys.length;
-//     let metaActive = meta.active;
+function draw(here, whereto){
+    let h = Number(here);
+    let w = Number(whereto);
 
-//     for (i = 0; i < numChars; i++){
-//         charKeys[i].innerText = NSEW_layout[Number(charKeys[i].id)][metaActive];
-//     }
-// }
+    document.getElementById(h.toString()).className =
+    document.getElementById(h.toString()).className.replace( /(?:^|\s)active(?!\S)/g , '' );
 
+    document.getElementById(w.toString()).className += " active";
+}
 
-// function walk(event){
-
-//     let press = event.key;
-//     let active = document.getElementsByClassName("active")[0];
-//     let activeId = active.id;
-//     let neighbor;
-//     let selected = null;
-
-//     if (press == controls.up){
-//         neighbor = 0;
-//     }
-//     else if (press == controls.right){
-//         neighbor = 1;
-//     }
-//     else if (press == controls.down){
-//         neighbor = 2;
-//     }
-//     else if (press == controls.left){
-//         neighbor = 3;
-//     }
-//     else if (press == controls.enter){
-//         selected = NSEW_layout[Number(activeId)];
-//         select(selected);
-//         if (selected.includes("Bksp")){
-//             neighbor = 28;
-//         }
-//         else {
-//             neighbor = 26;
-//         }
-//     }
-
-//     let jumpto;
-
-//     if (neighbor == 26){
-//         let jumpto = neighbor;
-//     }
-//     else {
-//         let jumpto = paths[Number(activeId)][neighbor];
-//     }
-
-//     if (jumpto >= 0){
-//         draw(activeId, jumpto);
-//     }
-// }
-
-
-// function select(key){
-
-//     if (key.includes("Meta")){
-//         meta.alter();
-//         populate();
-//     }
-//     else{
-//         let key = key[meta.active];
-//         let kbd = document.getElementsByClassName("NSEW_input")[0];
-//         let kbdLen = kbd.length;
-//     }
-
-//     switch(key){
-//         case "Bksp":
-//             kbd.value = kbd.value.slice(0,-1);
-//             break;
-//         case "Cls":
-//             kbd.value = kbd.value.slice(0, kbdLen-1);
-//             break;
-//         default:
-//             kbd.value += key;
-//             break;
-//     }
-// }
-
-
-// function draw(here, whereto){
-
-//     let h = Number(here);
-//     let w = Number(whereto);
-
-//     document.getElementById(h.toString()).className =
-//     document.getElementById(h.toString()).className.replace( /(?:^|\s)active(?!\S)/g , '' );
-
-//     document.getElementById(w.toString()).className += " active";
-// }
+populate();
+document.addEventListener("keydown", walk);
